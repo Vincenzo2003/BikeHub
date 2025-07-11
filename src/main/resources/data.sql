4f3f76b254b3a0c688e62a8b09a8f49e285016d2
v.guarino <v.guarino@namirial.com>
2025-07-11 16:23:03 +0200

    fix: sql scripts idempotency

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO PARKING_LOT (address, name) VALUES
-    ('Via dei Test', 'TestParkingLot');
+    ('Via dei Test', 'TestParkingLot')
+    ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO PARKING_LOT (address, name) VALUES
-    ('Via dei Test', 'TestParkingLot2');
+    ('Via dei Test', 'TestParkingLot2')
+    ON CONFLICT (name) DO NOTHING;
